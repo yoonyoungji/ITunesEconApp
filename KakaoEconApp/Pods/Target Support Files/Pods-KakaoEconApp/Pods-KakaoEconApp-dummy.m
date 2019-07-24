0ce251a3f27e7f33077d3cc5e31a4330ea1ecc63
youngji <dbsdudwl94@douzone.com>
2019-07-24 17:01:39 +0900

    [TestBranch] 테스트

---
+++ KakaoEconApp/Pods/Target Support Files/Pods-KakaoEconApp/Pods-KakaoEconApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KakaoEconApp : NSObject
+@end
+@implementation PodsDummy_Pods_KakaoEconApp
+@end
---
+++ KakaoEconApp/Pods/Target Support Files/Pods-KakaoEconAppTests/Pods-KakaoEconAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KakaoEconAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_KakaoEconAppTests
+@end
